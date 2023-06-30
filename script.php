a6d2e93eeca5dd8261e0852c6b8c6b16805249ed
S-Lombardi <silo.lombardi@gmail.com>
2023-06-30 15:39:42 +0200

    Stampo a schermo anche il testo censurato e la sua lunghezza

--- script.php
+++ script.php
@@ -1,16 +1,23 @@
 <?php
 
-//variabili per i valori inseriti nei campi input dall'utente
+//variabili per i valori inseriti nei campi input
 $paragraph = $_GET['paragraph'];
-$bad_word  = $_GET['bad_word']; 
+$bad_word  = $_GET['bad_word'];
 
 //funzione che calcola la lunghezza di una stringa
 $paragraph_length = strlen($paragraph);
 
 //stampo a schermo il paragrafo e la sua lunghezza
-echo $paragraph."<br>".$paragraph_length; 
+echo $paragraph." || lunghezza testo: ".$paragraph_length."<br><br><br>"; 
 
+/*Funzione che sostituisce caratteri con altri in una stringa
+ (da sostituire, il sostituto, la stringa)
+*/
+$censored_paragraph = str_replace($bad_word, "***" ,$paragraph );
 
-//Stamppo 
+//Ricalcolo la lunghezza della stringa censurata
+$censored_paragraph_length = strlen($censored_paragraph);
 
+//Stampo a schermo il paragrafo censurato e la sua lunghezza
+echo $censored_paragraph. " || lunghezza testo: " .$censored_paragraph_length;
 ?>
